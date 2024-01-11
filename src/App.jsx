838bb4049d6ac00a2176379dffba53d0e02c9612
Rajdeep <rajdeep.hascode@gmail.com>
2024-01-11 15:59:02 +0530

    changed the height of top loading bar and chnages in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       <BrowserRouter>
         <LoadingBar
           color='#f11946'
+          height={3}
           progress={progress}
           onLoaderFinished={() => setProgress(0)}
         />
